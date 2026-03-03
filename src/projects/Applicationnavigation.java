509214f356383137170938332f09cab0476227d5
Sreekesh Tolasi <tolasisreekesh18@gmail.com>
2026-03-03 10:11:25 +0000

    removed the navigation and deleted the unused module

--- src/projects/Applicationnavigation.java
+++ src/projects/Applicationnavigation.java
@@ -32,7 +32,7 @@ public class Applicationnavigation {
 		values.selectByVisibleText("Consultant");
 		driver.findElement(By.cssSelector("input[type='checkbox']")).click();
 		driver.findElement(By.id("signInBtn")).click();
-		driver.findElement(By.cssSelector("a[class='nav-link btn btn-primary']")).click();
+		
 			
 		
 		
